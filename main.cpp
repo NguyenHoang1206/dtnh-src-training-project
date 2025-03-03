4c564060a029e1db63102a9d28554d64410645ac
NguyenHoang1206 <daotruongnguyenhoang1208@gmail.com>
2025-03-03 11:10:02 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,13 +10,13 @@
 // Pins for all inputs, keep in mind the PWM defines must be on PWM pins
 // the default pins listed are the ones used on the Redbot (ROB-12097) with
 // the exception of STBY which the Redbot controls with a physical switch
-#define AIN1 18
-#define BIN1 21
-#define AIN2 19
-#define BIN2 22
-#define PWMA 15
-#define PWMB 16
-#define STBY 13
+#define AIN1 16
+#define BIN1 5
+#define AIN2 4
+#define BIN2 18
+#define PWMA 2
+#define PWMB 19
+#define STBY 17
  
 // these constants are used to allow you to make your motor configuration 
 // line up with function names like forward.  Value can be 1 or -1
@@ -34,6 +34,8 @@ void setup()
 {
  //Nothing here
  Dabble.begin("MyEsp32");  
+ Serial.begin(115200);
+ Serial.println("Dabble Begin");
 }
  
  
@@ -45,70 +47,59 @@ void loop()
    //backwards.  Speed can be from -255 to 255.  Also use of the 
    //brake function which takes no arguements.
 
-   if (GamePad.isUpPressed())
+   if (GamePad.isUpPressed())         // Di len
   {
-      motor1.drive(255,1000);
-      motor1.brake();
-      motor2.drive(255,1000);
-      motor2.brake();
-      delay(1000);
+    forward(motor1, 255);
+    forward(motor2, -255);
+    Serial.println("Forward");
+    //brake(motor1, motor2);
+    Serial.println("stop");
   }
 
   if (GamePad.isDownPressed())
   {
-      motor1.drive(-255,1000);
-      motor1.brake();
-      motor2.drive(-255,1000);
-      motor2.brake();
-      delay(1000);
+    back(motor1, -255);
+    back(motor2, 255);
+    Serial.println("Back");
+    //brake(motor1, motor2);
+    Serial.println("stop");
   }
 
-  if (GamePad.isLeftPressed()) 
+  if (GamePad.isLeftPressed())
   {
-      motor2.drive(255,1000);
-      motor2.brake();
-      delay(1000);
+    left(motor1, motor2, 255);
+    Serial.println("Left");
+    //brake(motor1, motor2);
+    Serial.println("stop");
   }
 
   if (GamePad.isRightPressed())
   {
-      motor1.drive(255,1000);
-      motor1.brake();
-      delay(1000);
+    right(motor1, motor2, 255);
+    Serial.println("Right");
+    //brake(motor1, motor2);
+    Serial.println("stop");
   }
 
   if (GamePad.isSquarePressed())
   {
-      motor2.drive(255,1000);
-      motor2.drive(-255,1000);
-      motor2.brake();
-      delay(1000);
+
   }
 
   if (GamePad.isCirclePressed())
   {
-      motor1.drive(255,1000);
-      motor1.drive(-255,1000);
-      motor1.brake();
-      delay(1000);
+
   }
 
   if (GamePad.isCrossPressed())
   {
-      motor1.drive(-255,1000);
-      motor1.brake();
-      motor2.drive(-255,1000);
-      motor2.brake();
-      delay(1000);
+    brake(motor1, motor2);
+    Serial.println("Brake");
   }
 
   if (GamePad.isTrianglePressed())
   {
-      motor1.drive(255,1000);
-      motor1.brake();
-      motor2.drive(255,1000);
-      motor2.brake();
-      delay(1000);
+
   }
 
   if (GamePad.isStartPressed())
@@ -121,6 +112,10 @@ void loop()
 
   }
 
+  else 
+  {
+    brake(motor1, motor2);
+  }
    
 }
 
@@ -137,27 +132,42 @@ void test(){
    //it will go backwards
    if (GamePad.isUpPressed())
   {
-   forward(motor1, motor2, 150);
+   forward(motor1, 150);
+   forward(motor2,-150);
    delay(1000);
   }
    //Use of the back function, which takes as arguments two motors 
    //and optionally a speed.  Either a positive number or a negative
    //number for speed will cause it to go backwards
-   back(motor1, motor2, -150);
+   if(GamePad.isDownPressed())
+   {
+   back(motor1, -150);
+   back(motor2, 150);
    delay(1000);
+   }
    
    //Use of the brake function which takes as arguments two motors.
    //Note that functions do not stop motors on their own.
+   if (GamePad.isCrossPressed())
+   {
    brake(motor1, motor2);
    delay(1000);
-   
+   }
+
    //Use of the left and right functions which take as arguements two
    //motors and a speed.  This function turns both motors to move in 
    //the appropriate direction.  For turning a single motor use drive.
-   left(motor1, motor2, 100);
+   if(GamePad.isLeftPressed())
+   {
+   left(motor1, motor2, 150);
    delay(1000);
-   right(motor1, motor2, 100);
+   }
+
+   if(GamePad.isRightPressed())
+   {
+   right(motor1, motor2, 150);
    delay(1000);
+   }
    
    //Use of brake again.
    brake(motor1, motor2);
